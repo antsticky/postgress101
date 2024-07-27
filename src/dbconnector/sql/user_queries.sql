e77534b9d3f67ce3ca5eb7163d6da85586d439f8
Sinkovicz Péter <antsticky@Sinkovicz-MacBook-Air.local>
2024-07-27 18:27:03 +0200

    add queries

---
+++ src/dbconnector/sql/user_queries.sql
@@ -0,0 +1,23 @@
+-- Select all columns from all rows
+SELECT *
+FROM users;
+
+-- Select specific columns (name and position) from all rows
+SELECT name, position
+FROM users;
+
+-- Select all users who are at least 25 years old
+SELECT *
+FROM users
+WHERE age >= 25;
+
+-- Select all users and order by age in ascending order
+SELECT *
+FROM users
+ORDER BY age ASC;
+
+-- Select the top 5 oldest users
+SELECT *
+FROM users
+ORDER BY age DESC
+LIMIT 5;
