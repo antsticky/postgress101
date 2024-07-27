100de47a42fdf9dd4b4f3fa194f728b161c40800
Sinkovicz Péter <antsticky@Sinkovicz-MacBook-Air.local>
2024-07-27 18:04:06 +0200

    bad

--- src/dbconnector/sql/create_table.sql
+++ src/dbconnector/sql/create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+name VARCHAR(255) NOT NULL,
     age INTEGER NOT NULL CHECK (age >= 0),
 position VARCHAR(100) NOT NULL
 );
