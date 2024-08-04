1d03f1e37c0f6295a8f6ceaf87839a1b3361bcdf
Sinkovicz Péter <antsticky@sinkovicz-air.t.hu>
2024-08-04 12:43:25 +0200

    add drop if exists

--- src/dbconnector/sql/new.sql
+++ src/dbconnector/sql/new.sql
@@ -1,5 +1,7 @@
+DROP FUNCTION IF EXISTS SelectUserByAge(INTEGER, VARCHAR);
+
 CREATE OR REPLACE FUNCTION SelectUserByAge(age_limit INTEGER, name_lookup VARCHAR(255))
-RETURNS TABLE(id INT, name VARCHAR, age INT) AS $$
+RETURNS SETOF users AS $$
 BEGIN
     RETURN QUERY
     SELECT * FROM users WHERE users.name = name_lookup AND users.age > age_limit;
@@ -7,4 +9,4 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-SELECT * FROM SelectUserByAge(10, "Alice Johnson");
+SELECT * FROM SelectUserByAge(10, 'Alice Johnson');
