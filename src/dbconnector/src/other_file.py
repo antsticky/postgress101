1b2615fff7a07051300a3041312a944ce72f60c8
Sinkovicz Péter <antsticky@sinkovicz-air.t.hu>
2024-08-04 18:10:28 +0200

    add memory usage decorator

--- src/dbconnector/src/other_file.py
+++ src/dbconnector/src/other_file.py
@@ -8,10 +8,12 @@ db_logger = get_db_logger(
     level=logging.DEBUG,
 )
 
-
+@pm.memory_usage_logger(db_logger)
 @pm.time_logger(db_logger)
 def src_fn():
-    db_logger.info("Inside src fn")
+    db_logger.debug("Inside src fn")
     import time
 
     time.sleep(1)
+    
+    large_string = "a"*1024*1024*64
