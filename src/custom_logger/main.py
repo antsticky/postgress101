a894cbc74bb2fdb1f4b66988cfb00e129e78457c
Sinkovicz Péter <antsticky@sinkovicz-air.t.hu>
2024-08-31 11:56:33 +0200

    add db logger extra params

--- src/custom_logger/main.py
+++ src/custom_logger/main.py
@@ -1,6 +1,7 @@
 import logging
 
 
+from custom_logger.src.data_models.extras import DBLoggerExtras, TableName
 from custom_logger.src.handlers.db import SpecialHandler
 from custom_logger.src.handlers.std import ConsoleLogHandler
 
@@ -9,26 +10,26 @@ from custom_logger.src.logger import get_logger
 
 def main():
     console_log_handler = ConsoleLogHandler(log_level=logging.DEBUG, log_format="[%(levelname)s] %(asctime)s | %(name)s - %(message)s")
-    db_log_handler = SpecialHandler(log_level=logging.DEBUG)
+    
+    db_logger_extra_params = DBLoggerExtras(
+        host="0.0.0.0",
+        port=1234,
+        user="root",
+        password="password",
+        db="test_db",
+        table_names=TableName(
+            info="info_table",
+            warning="warning_table",
+            error="error_table"
+        ),
+    )
+    db_log_handler = SpecialHandler(log_level=logging.DEBUG, extra_params=db_logger_extra_params)
 
     logger = get_logger(__name__, handlers=[console_log_handler, db_log_handler])
 
     logger.info("test msg1")
     logger.info("test msg2", extra={"structured_data": {"my_key": "value"}})
     logger.info("test msg3", extra={"structured_data": {"my_key": "value"}})
-
-    # logger = get_logger(
-    #     __name__,
-    #     extras=DataModels.DBLoggerExtras(
-    #         host="0.0.0.0",
-    #         port=1234,
-    #         user="root",
-    #         password="password",
-    #         db="test_db"),
-    #     logger_type="db_logger",
-    #     level=logging.DEBUG,
-    # )
-    # logger.info("This is an info message")
 
 
 if __name__ == "__main__":
--- src/custom_logger/src/handlers/db.py
+++ src/custom_logger/src/handlers/db.py
@@ -1,14 +1,14 @@
 import logging
 
 from custom_logger.src.handlers.base import BaseLogHandler
-
+from custom_logger.src.data_models.extras import DBLoggerExtras
 
 class SpecialHandler(BaseLogHandler, logging.Handler):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, extra_params: DBLoggerExtras, *args, **kwargs):
+        self.extra_params = extra_params
         super().__init__(*args, **kwargs)
 
     def emit(self, record):
-        print(dir(record))
         print(record.levelname)
         print(record.processName)
         print(record.process)
@@ -17,6 +17,7 @@ class SpecialHandler(BaseLogHandler, logging.Handler):
         print(record.module)
         print(record.lineno)
         print(record.getMessage())
+        print(self.extra_params)
         
         structured_data = getattr(record, "structured_data", {})
         print(structured_data.get("my_key"))
